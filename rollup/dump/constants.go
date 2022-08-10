53fd720fd452c3d1ce14ff1be2af511f9ffb7ee7
benVmeta3 <ben@vmeta.com>
2022-08-11 02:56:52 +0800

    Genesis

---
+++ rollup/dump/constants.go
@@ -0,0 +1,9 @@
+package dump
+
+import (
+	"github.com/VMETA3/vmeta3-l2geth/common"
+)
+
+var OvmEthAddress = common.HexToAddress("0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000")
+var OvmFeeWallet = common.HexToAddress("0x4200000000000000000000000000000000000011")
+var OvmWhitelistAddress = common.HexToAddress("0x4200000000000000000000000000000000000002")
